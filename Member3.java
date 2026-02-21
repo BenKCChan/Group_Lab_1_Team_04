996bc06e3e07394c82f6fa35afd5172446e50d0b
krystinf518 <falcone.kr@northeastern.edu>
2026-02-21 14:28:09 -0500

    Add Member3.java

---
+++ Member3.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package group_lab_1_team_04;
+
+/**
+ *
+ * @author krystinfalcone
+ */
+public class Member3 {
+
+public static void main(String[] args) {
+
+        System.out.println("Hello from Member 3!");
+
+    }
+
+}
